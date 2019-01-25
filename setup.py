121fb5eb35bdb8784693f69e75bbb80877f1abc7
Alexander Shpilkin <ashpilkin@gmail.com>
2019-01-25 22:34:14 +0300

    Eliminate pytest-runner

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
-import setuptools
+from setuptools              import setup
+from setuptools.command.test import test as Test
+
+class CustomTest(Test):
+	user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+	def initialize_options(self):
+		super().initialize_options()
+		self.pytest_args = " ".join([
+			"-v",
+			"--cov",
+			"--cov-report term:skip-covered"
+			"--cov-report annotate"
+		])
+
+	def run_tests(self):
+		from shlex  import split
+		from sys    import exit
+		from pytest import main
+
+		exit(main(split(self.pytest_args)))
 
 with open('README.rst', 'r') as fp:
 	for line in fp:
 		if not line[:-1]: break
 	readme = fp.read()
 
-setuptools.setup(
+setup(
 	name='po',
 	version='0.1.0',
 	author='Alexander Shpilkin',
@@ -14,11 +34,6 @@ setuptools.setup(
 	long_description=readme,
 	long_description_content_type='text/x-rst',
 	url='https://github.com/alexshpilkin/po',
-	py_modules=['po'],
-	python_requires='~=3.0',
-	install_requires=[],
-	setup_requires=['pytest-runner'],
-	tests_require=['pytest', 'pytest-cov'],
 	classifiers=[
 		'Development Status :: 3 - Alpha',
 		'Intended Audience :: Developers',
@@ -29,4 +44,12 @@ setuptools.setup(
 		'Topic :: Software Development :: Localization',
 		'Topic :: Text Processing :: Linguistic',
 	],
+
+	py_modules=['po'],
+	python_requires='~=3.0',
+	install_requires=[],
+	tests_require=['pytest', 'pytest-cov'],
+	cmdclass={
+		'test': CustomTest,
+	}
 )
